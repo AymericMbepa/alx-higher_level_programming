8398b75286089dba7a13e29f3508a2fe094370f4
AymericMbepa <aymericmbepa@gmail.com>
2022-11-30 11:58:58 -0800

    prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def uppercase(str):
+    """prints a string in uppercase followed by a new line"""
+    for i in str:
+        if ord(i) > 96:
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end='')
+    print()
