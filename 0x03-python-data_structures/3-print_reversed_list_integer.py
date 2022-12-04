2c0ba5d0c773d8c2e642de408d9039531ef2b800
AymericMbepa <aymericmbepa@gmail.com>
2022-12-04 05:39:14 -0800

    prints all integers of a list, in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order"""
-    if len(my_list) == 0:
+    if my_list is None:
         return None
     i = -1
     while i >= -len(my_list):
