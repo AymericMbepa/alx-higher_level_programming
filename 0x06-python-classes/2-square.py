164434925d28646e52ed10a82f600b071bc653cd
AymericMbepa <aymericmbepa@gmail.com>
2022-12-21 12:05:05 -0800

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@autor: Aymeric Mbepa
+"""
+
+
+class Square:
+    """Define Square"""
+
+
+    def __init__(self, size=0):
+        """
+        the __init__ method for a square class
+
+        args:
+        size: a private instance size
+
+        Raises:
+          TypeError: Exception if size is not an integer
+          ValueError: Exception if size is less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
