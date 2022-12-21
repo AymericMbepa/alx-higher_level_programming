afe310f2be648a684951e121079cb26450eeb850
AymericMbepa <aymericmbepa@gmail.com>
2022-12-21 12:49:25 -0800

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@Author: Aymeric Mbou
+"""
+
+class Square:
+    """ Define Square
+
+    Attributes:
+    size: size of square
+
+    """
+    def __init__(self, size=0):
+        """ the init methot for Square class
+
+        Args:
+          size: the size of square
+
+        Raise:
+          TypeError: Exceptin if size is not an integer
+          ValueError: Exception if size is less than 0
+
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """ function that computr area of square
+
+        Returns:
+          the current square area
+
+        """
+        return self.__size ** 2
