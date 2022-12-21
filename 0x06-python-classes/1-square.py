46eaac0283a4965284d19f4a692fd43e20294dae
AymericMbepa <aymericmbepa@gmail.com>
2022-12-21 11:03:26 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square by Aymeric Mbepa
+"""Define square"""
+
+
+class Square:
+    """Represent square"""
+
+    def ___init__(self, size):
+        """ Initializing this square class
+        agrs: size - represent the size of this square defined
+        """
+
+        self.__size = size
