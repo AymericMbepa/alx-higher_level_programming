dbe2dde98a16f44400e77bead788d8f45686f26d
AymericMbepa <aymericmbepa@gmail.com>
2023-01-09 12:29:40 -0800

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+empty class
+
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    def area(self):
+        """method not implemend yet"""
+        raise Exception("area() is not implemented")
