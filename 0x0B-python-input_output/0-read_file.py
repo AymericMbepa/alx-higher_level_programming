b7f5afca50d766f81898eac4a6966bb083fb3903
AymericMbepa <aymericmbepa@gmail.com>
2023-01-10 01:09:12 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ this module read a text file
 
 
 def read_file(filename=""):
-    """read text file"""
+    """read text file                              """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end ='')
