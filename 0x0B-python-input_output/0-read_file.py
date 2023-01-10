dc8d588d7d7330b9baf83b5148f22298a9e2839c
AymericMbepa <aymericmbepa@gmail.com>
2023-01-10 01:16:47 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
 this module read a text file
+@AUTHOR: Aymeric Mbepa
 
 """
 
 
 def read_file(filename=""):
-    """read text file                              """
+    """
+    read text file
+    Arguments:
+        filename: the name of the file to open
+    """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end ='')
