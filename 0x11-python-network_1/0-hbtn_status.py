b575f65799d14ae0e4e891d5bf4acf6a80dce500
AymericMbepa <aymericmbepa@gmail.com>
2023-04-04 13:19:58 -0700

    fetches url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""
- fetches https://alx-intranet.hbtn.io/status
 
 """
+Fetches https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html.decode('utf-8')))
