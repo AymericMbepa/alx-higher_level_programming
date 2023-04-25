fd8cda5a18ecc44a14093cd3a05a0b26b0642cbf
AymericMbepa <aymericmbepa@gmail.com>
2023-04-25 09:13:22 -0700

    statut code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-
-'use strict';
-
 const request = require('request');
-
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-    if (error) {
-	console.error(error);
-	return;
-    }
-
-    console.log('code: ${response.statusCode}');
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
